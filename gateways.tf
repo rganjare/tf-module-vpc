5d6e46d2a028fcb60409970979d95867920e7cf8
rganjare <rahul.ganjare@gmail.com>
2022-06-23 00:32:28 +0530

    updated

---
+++ gateways.tf
@@ -0,0 +1,21 @@
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.ENV}-igw"
+  }
+}
+
+# resource "aws_eip" "ngw-eip" {
+#   vpc = true
+# }
+
+# resource "aws_nat_gateway" "ngw" {
+#   allocation_id = aws_eip.ngw-eip.id
+#   subnet_id     = aws_subnet.public.*.id[0]
+
+#   tags = {
+#     Name = "${var.ENV}-ngw"
+#   }
+#   depends_on = [aws_internet_gateway.igw]
+# }
