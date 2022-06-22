f87ce1036427e9c50fc8dfd38d3ac69dcce546d7
rganjare <rahul.ganjare@gmail.com>
2022-06-22 18:54:49 +0530

    updated

---
+++ subnet.tf
@@ -0,0 +1,21 @@
+resource "aws_subnet" "public" {
+  count             = length(var.PUBLIC_SUBNET_CIDR)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.PUBLIC_SUBNET_CIDR, count.index)
+  availability_zone = element(var.AZ, count.index)
+
+  tags = {
+    Name = "${var.ENV}-pub-${element(var.AZ, count.index)}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.PRIVATE_SUBNET_CIDR)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.PRIVATE_SUBNET_CIDR, count.index)
+  availability_zone = element(var.AZ, count.index)
+
+  tags = {
+    Name = "${var.ENV}-priv-${element(var.AZ, count.index)}"
+  }
+}
--- vars.tf
+++ vars.tf
@@ -1,2 +1,12 @@
 variable "VPC_CIDR" {}
 variable "ENV" {}
+variable "PUBLIC_SUBNET_CIDR" {}
+variable "PRIVATE_SUBNET_CIDR" {}
+variable "AZ" {}
+variable "DEFAULT_VPC_ID" {}
+variable "DEFAULT_VPC_CIDR" {}
+variable "DEFAULT_VPC_RT" {}
+variable "HOSTEDZONE_PRIVATE_ID" {}
+variable "HOSTEDZONE_PRIVATE_ZONE" {}
+variable "HOSTEDZONE_PUBLIC_ID" {}
+variable "HOSTEDZONE_PUBLIC_ZONE" {}
